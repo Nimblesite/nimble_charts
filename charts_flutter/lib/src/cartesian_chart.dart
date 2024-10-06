10e5cd94dd4533cc9772be7bde58c45d33e8e247
Sanjana Rawal <sanjanarawal2552001@gmail.com>
2024-10-06 19:42:44 +0530

    Update cartesian_chart.dart
    #33 please let me know if this is how you wanted the documentation.

--- charts_flutter/lib/src/cartesian_chart.dart
+++ charts_flutter/lib/src/cartesian_chart.dart
@@ -14,15 +14,34 @@
 // limitations under the License.
 
 import 'dart:collection' show LinkedHashMap;
-
 import 'package:meta/meta.dart' show immutable, protected;
 import 'package:nimble_charts/src/base_chart.dart' show BaseChart;
 import 'package:nimble_charts/src/base_chart_state.dart' show BaseChartState;
 import 'package:nimble_charts_common/common.dart' as common
     show AxisSpec, BaseChart, CartesianChart, NumericAxis, NumericAxisSpec;
 
+/// Base class for all cartesian charts.
+///
+/// A cartesian chart is a chart that uses a cartesian coordinate system - it
+/// has a domain axis and a measure axis.
 @immutable
 abstract class CartesianChart<D> extends BaseChart<D> {
+  /// Creates a [CartesianChart].
+  ///
+  /// [seriesList] List of series of data this chart will visualize.
+  /// [domainAxis] Specification for the domain axis.
+  /// [primaryMeasureAxis] Specification for the primary measure axis.
+  /// [secondaryMeasureAxis] Specification for the secondary measure axis.
+  /// [disjointMeasureAxes] Specifications for any disjoint measure axes.
+  /// [defaultRenderer] Default renderer used to draw series data on the chart.
+  /// [customSeriesRenderers] Custom renderers used to draw series data.
+  /// [behaviors] List of behaviors to attach to this chart.
+  /// [selectionModels] List of selection models used in this chart.
+  /// [rtlSpec] Configures the chart to be RTL.
+  /// [defaultInteractions] Whether to use the default interactions.
+  /// [layoutConfig] Layout configuration for the chart.
+  /// [userManagedState] User managed state for the chart.
+  /// [flipVerticalAxis] Whether to flip the vertical axis direction.
   const CartesianChart(
     super.seriesList, {
     super.key,
@@ -42,10 +61,20 @@ abstract class CartesianChart<D> extends BaseChart<D> {
     super.userManagedState,
     this.flipVerticalAxis,
   });
+
+  /// The domain axis for this chart (usually the x-axis).
   final common.AxisSpec? domainAxis;
+
+  /// The primary measure axis for this chart (usually the y-axis).
   final common.NumericAxisSpec? primaryMeasureAxis;
+
+  /// The secondary measure axis for this chart.
   final common.NumericAxisSpec? secondaryMeasureAxis;
+
+  /// Map of axis ID to measure axis spec for all disjoint measure axes.
   final LinkedHashMap<String, common.NumericAxisSpec>? disjointMeasureAxes;
+
+  /// Whether to flip the vertical axis direction.
   final bool? flipVerticalAxis;
 
   @override
@@ -55,7 +84,6 @@ abstract class CartesianChart<D> extends BaseChart<D> {
     BaseChartState<D> chartState,
   ) {
     super.updateCommonChart(chart, oldWidget, chartState);
-
     final prev = oldWidget as CartesianChart?;
     final cartesianChart = chart as common.CartesianChart;
 
@@ -84,16 +112,18 @@ abstract class CartesianChart<D> extends BaseChart<D> {
     }
   }
 
+  /// Creates a map of axis IDs to [common.NumericAxis] for disjoint measure axes.
+  ///
+  /// This method is used internally to create numeric axes for disjoint measures
+  /// based on the specifications provided in [disjointMeasureAxes].
   @protected
   LinkedHashMap<String, common.NumericAxis>? createDisjointMeasureAxes() {
     if (disjointMeasureAxes != null) {
       // ignore: prefer_collection_literals
       final disjointAxes = LinkedHashMap<String, common.NumericAxis>();
-
       disjointMeasureAxes!.forEach((axisId, axisSpec) {
         disjointAxes[axisId] = axisSpec.createAxis();
       });
-
       return disjointAxes;
     } else {
       return null;
